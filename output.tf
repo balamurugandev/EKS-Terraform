35b7b2e405e588d5c30ef27f44826b62b90b1ce2
Balamurugan <108758172+balamurugandev@users.noreply.github.com>
2024-11-23 15:37:29 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,16 +1,15 @@
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.balamurugan.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.balamurugan.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.balamurugan_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.balamurugan_subnet[*].id
 }
-
